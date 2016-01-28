57e73d86303900acfc0fb31e0cb0f1131e99dc6c
Laurent Peuch <cortex@worlddomination.be>
2016-01-29 00:31:03 +0100

    [enh] add a view that list all pads of an orga

--- meetings/urls.py
+++ meetings/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
+    url(r'^meeting/$', views.MeetingList.as_view(), name='meeting_list'),
 ]
--- meetings/views.py
+++ meetings/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
 from .models import Orga
 
@@ -7,3 +8,16 @@ def home(request):
     return render(request, "home.haml", {
         "orga": Orga.get_orga_from_request(request)
     })
+
+
+class MeetingList(ListView):
+    template_name = "meetings/meeting_list.haml"
+
+    def get_queryset(self):
+        self.orga = Orga.get_orga_from_request(self.request)
+        return self.orga.meeting_set.all()
+
+    def get_context_data(self):
+        context = super(MeetingList, self).get_context_data()
+        context["orga"] = self.orga
+        return context
