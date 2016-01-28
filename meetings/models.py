03d470f6e02cb402422537b7ec330ff2802d2d3d
Laurent Peuch <cortex@worlddomination.be>
2016-01-28 23:34:12 +0100

    [enh] rerender a pad when the format changes

--- meetings/models.py
+++ meetings/models.py
@@ -86,6 +86,12 @@ class Meeting(models.Model):
             if maybe_title:
                 self.title = maybe_title[0]
 
+        if self.id:
+            in_db = Meeting.objects.get(id=self.id)
+
+            if in_db.format != self.format:
+                self.render_content()
+
         return super(Meeting, self).save(*args, **kwargs)
 
     def __unicode__(self):
