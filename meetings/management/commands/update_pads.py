56b79b9c513f91a0e4c32a641d4f03f1845e575e
Laurent Peuch <cortex@worlddomination.be>
2016-01-29 20:59:13 +0100

    [enh] add a command to update pads

---
+++ meetings/management/commands/update_pads.py
@@ -0,0 +1,18 @@
+import sys
+import traceback
+
+from django.core.management.base import BaseCommand
+
+from meetings.models import Meeting
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        for pad in Meeting.objects.all():
+            print "[%s]" % pad
+            try:
+                pad.update_content()
+                pad.render_content()
+            except Exception:
+                traceback.print_exc(file=sys.stdout)
+                print "^ on %s" % pad
