3ac99b90e675b852d45ba2386601203f0191b1a2
Laurent Peuch <cortex@worlddomination.be>
2016-01-29 01:22:44 +0100

    [fix] include slug into meeting url

--- meetings/urls.py
+++ meetings/urls.py
@@ -6,5 +6,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'^meeting/$', views.MeetingList.as_view(), name='meeting_list'),
-    url(r'^meeting/(?P<pk>\d+)-.+/$', views.MeetingDetail.as_view(), name='meeting_detail'),
+    url(r'^meeting/(?P<pk>\d+)-(.+)/$', views.MeetingDetail.as_view(), name='meeting_detail'),
 ]
