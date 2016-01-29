2e5475ff34ec622e3d89fc124d2faaf41ee6d45c
Laurent Peuch <cortex@worlddomination.be>
2016-01-29 01:23:37 +0100

    [fix] I want to last part for a slug

--- meetings/models.py
+++ meetings/models.py
@@ -87,7 +87,7 @@ class Meeting(models.Model):
                 self.title = maybe_title[0]
 
         if not self.slug:
-            self.slug = filter(None, self.url.split("/"))[0]
+            self.slug = filter(None, self.url.split("/"))[-1]
 
         if self.id:
             in_db = Meeting.objects.get(id=self.id)
